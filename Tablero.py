a800e7e8558870ddf81c54b78cde2679cc453138
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-07 20:36:37 +0100

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -10,8 +10,9 @@ c2 = "caballo blanca"
 a2 = "alfil blanca"
 k2 = "rey blanca"
 q2 = "reina blanca"
-tablero = [[t1, c1, a1, k1, q1, a1, c1, t1], [p1, p1, p1, p1, p1, p1, p1, p1], [], [], [], [], [p2, p2, p2, p2, p2, p2, p2, p2], [t2, c2, a2, k2, q2, a2, c2, t2]]
+e = "espacio"
+tablero = [[t1, c1, a1, k1, q1, a1, c1, t1], [p1, p1, p1, p1, p1, p1, p1, p1], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [p2, p2, p2, p2, p2, p2, p2, p2], [t2, c2, a2, k2, q2, a2, c2, t2]]
 def desplegar_tablero():
     for i in tablero:
-        print(tablero[i])
+        print(i)
 desplegar_tablero()
