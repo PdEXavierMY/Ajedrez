3a2595029d1c2f500ea4f7876bdef1c5208f9713
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-07 20:46:34 +0100

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,18 +1,24 @@
-p1 = "peón negro"
-t1 = "torre negro"
-c1 = "caballo negro"
-a1 = "alfil negro"
-k1 = "rey negro"
-q1 = "reina negro"
-p2 = "peón blanco"
-t2 = "torre blanca"
-c2 = "caballo blanca"
-a2 = "alfil blanca"
-k2 = "rey blanca"
-q2 = "reina blanca"
-e = "espacio"
-tablero = [[t1, c1, a1, k1, q1, a1, c1, t1], [p1, p1, p1, p1, p1, p1, p1, p1], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [e, e, e, e, e, e, e, e], [p2, p2, p2, p2, p2, p2, p2, p2], [t2, c2, a2, k2, q2, a2, c2, t2]]
-def desplegar_tablero():
+figuras_negras = {
+    'p1' : "peón negro",
+    't1' : "torre negro",
+    'c1' : "caballo negro",
+    'a1' : "alfil negro",
+    'k1' : "rey negro",
+    'q1' : "reina negro",
+    'e' : "espacio",
+    }
+
+figuras_blancas = {
+    'p2' : "peón blanco",
+    't2' : "torre blanca",
+    'c2' : "caballo blanca",
+    'a2' : "alfil blanca",
+    'k2' : "rey blanca",
+    'q2' : "reina blanca",
+    'e' : "espacio",
+    }
+
+def desplegar_tablero(tablero):
     for i in tablero:
         print(i)
-desplegar_tablero()
+print(figuras.values())
