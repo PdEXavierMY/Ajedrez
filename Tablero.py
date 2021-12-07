98168ce987aa5c8e8713856d5d0604297a553e71
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-07 21:12:34 +0100

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,24 +1,30 @@
 figuras_negras = {
-    'p1' : "peón negro",
     't1' : "torre negro",
     'c1' : "caballo negro",
     'a1' : "alfil negro",
     'k1' : "rey negro",
     'q1' : "reina negro",
-    'e' : "espacio",
+    }
+
+peones_negros = {
+    'p1' : "peón negro",
     }
 
 figuras_blancas = {
-    'p2' : "peón blanco",
     't2' : "torre blanca",
     'c2' : "caballo blanca",
     'a2' : "alfil blanca",
     'k2' : "rey blanca",
     'q2' : "reina blanca",
-    'e' : "espacio",
+    'p2' : "peón blanco",
+    }
+
+peones_blancos = {
+    'p2' : "peón blanco",
     }
 
 def desplegar_tablero(tablero):
     for i in tablero:
         print(i)
-print(figuras.values())
+lista = list(figuras_negras)
+print(lista)
