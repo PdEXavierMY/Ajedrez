0939305e3c6a093e204c041cbb0aa8136a82901b
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-08 12:28:36 +0100

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -18,24 +18,14 @@ def desplegar_tablero():
         print(i)
 
 tablero = [["t1", "c1", "a1", "k1", "q1", "a1", "c1", "t1"], ["p1", "p1", "p1", "p1", "p1", "p1", "p1", "p1"], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["p2", "p2", "p2", "p2", "p2", "p2", "p2", "p2"], ["t2", "c2", "a2", "k2", "q2", "a2", "c2", "t2"]]
-print("El significado de cada símbolo es el siguiente" + str(figuras))
-desplegar_tablero()
-
 #Partimos suponiendo que conocemos las reglas del juego y sabemos que se puede y que no se puede hacer
 
-def mover_acabar1():
-    partida1 = True
-    decision1 = input("Jugador 1, ¿Quiere mover ficha(escriba 1) o terminar la partida(escriba 2)?")
-    if decision1 == 2:
-        partida1 = False
-    return partida1
-
-def mover_acabar2():
-    partida2 = True
-    decision2 = input("Jugador 2, ¿Quiere mover ficha(escriba 1) o terminar la partida(escriba 2)?")
-    if decision2 == 2:
-        partida2 = False
-    return partida2
+def mover_acabar():
+    partida = True
+    decision = input("¿Quiere mover ficha(escriba 1) o terminar la partida(escriba 2)?")
+    if decision == 2:
+        partida = False
+    return partida
 
 def mover_ficha():
     ficha = list(map(int, input("Eliga la ficha que quiere mover(usando las coordenadas): ").rstrip().split()))
@@ -49,7 +39,8 @@ def mover_ficha():
     return tablero
 
 def jugar_una_ronda(juego):
-    j1 = mover_acabar1()
+    print("Jugador 1: ", end = "")
+    j1 = mover_acabar()
     if j1 == True:
         print("Jugador 1:")
         mover_ficha()
@@ -58,7 +49,8 @@ def jugar_una_ronda(juego):
         mover_ficha()
         desplegar_tablero()
     else:
-        j2 = mover_acabar2()
+        print("Jugador 2: ", end = "")
+        j2 = mover_acabar()
         if j2 == True:
             print("Jugador 1, debe mover ficha:")
             mover_ficha()
@@ -72,9 +64,11 @@ def jugar_una_ronda(juego):
     return tablero and juego
 
 def jugar():
-    print("Vamos a jugar al ajedrez")
-    print("A continuación se desplegará el tablero")
+    print("Vamos a jugar al ajedrez.")
+    print("A continuación se desplegará el tablero.")
     print("Para mover ficha o referirse a una casilla deberán indicarla con coordenadas, leyendo el tablero de arriba a abajo, de izquierda a derecha, siendo la primera la casilla 0, 0, la segunda la 0, 1 y así hasta la 7, 7")
+    desplegar_tablero()
+    print("El significado de cada símbolo es el siguiente" + str(figuras))
     juego = True
     while juego:
         jugar_una_ronda(juego)
