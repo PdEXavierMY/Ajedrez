ded1739b0d301fee4b3584a4864ceb1199832ed5
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-08 12:23:37 +0100

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,24 +1,15 @@
-figuras_negras = {
+figuras = {
     't1' : "torre negro",
     'c1' : "caballo negro",
     'a1' : "alfil negro",
     'k1' : "rey negro",
     'q1' : "reina negro",
-    }
-
-peones_negros = {
     'p1' : "peón negro",
-    }
-
-figuras_blancas = {
     't2' : "torre blanca",
     'c2' : "caballo blanca",
     'a2' : "alfil blanca",
     'k2' : "rey blanca",
     'q2' : "reina blanca",
-    }
-
-peones_blancos = {
     'p2' : "peón blanco",
     }
 
@@ -26,18 +17,8 @@ def desplegar_tablero():
     for i in tablero:
         print(i)
 
-fichas_negras = list(figuras_negras)
-fichas_blancas = list(figuras_blancas)
-peon_negro = list(peones_negros)
-peon_blanco = list(peones_blancos)
-tablero = [[], [], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], [], []]
-tablero[0] = tablero[0] + fichas_negras + [fichas_negras[2]] + [fichas_negras[1]] + [fichas_negras[0]]
-tablero[7] = tablero[7] + fichas_blancas + [fichas_blancas[2]] + [fichas_blancas[1]] + [fichas_blancas[0]]
-for i in range(8):
-    tablero[1] += peon_negro
-    tablero[6] += peon_blanco
-
-print("El significado de cada símbolo es el siguiente" + str(figuras_negras) + str(peones_negros) + str(figuras_blancas) + str(peones_blancos))
+tablero = [["t1", "c1", "a1", "k1", "q1", "a1", "c1", "t1"], ["p1", "p1", "p1", "p1", "p1", "p1", "p1", "p1"], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ["p2", "p2", "p2", "p2", "p2", "p2", "p2", "p2"], ["t2", "c2", "a2", "k2", "q2", "a2", "c2", "t2"]]
+print("El significado de cada símbolo es el siguiente" + str(figuras))
 desplegar_tablero()
 
 #Partimos suponiendo que conocemos las reglas del juego y sabemos que se puede y que no se puede hacer
@@ -88,7 +69,7 @@ def jugar_una_ronda(juego):
         else:
             print("Gracias por jugar")
             juego = False
-    return tablero
+    return tablero and juego
 
 def jugar():
     print("Vamos a jugar al ajedrez")
